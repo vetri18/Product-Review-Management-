4187f76705f7c95d81457040f52d567e583cdd62
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-12 15:09:53 +0530

    UC2_TopRecords

---
+++ ProductManagement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ProductReviewManagement
+{
+    public class ProductManagement
+    {
+
+        public readonly DataTable dataTable = new DataTable();
+        /// <summary>
+        /// UC2 - selected records with order by clause.
+        /// </summary>
+        /// <param name="review">The review.</param>
+        public void TopRecords(List<ProductReview> listReview)
+        {
+            //using query syntax
+            var recordedData = (from productReviews in listReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+            //using lambda syntax
+            var recordData = listReview.OrderByDescending(r => r.Rating).Take(3);
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
+            }
+        }
+
+    }
+}

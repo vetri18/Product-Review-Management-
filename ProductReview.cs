8cba99a841d9c592a49df5fb9310738b204f5a03
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-12 15:12:08 +0530

    UC8_AddingDataTable

---
+++ ProductReview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class ProductReview
+    {
+
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+
+
+    }
+}

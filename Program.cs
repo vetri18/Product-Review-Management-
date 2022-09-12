44b44d8538cf8abf10ec409bf5781494df253631
vetrivel <vetrivel.kce@gmail.com>
2022-09-12 15:04:54 +0530

    welcome to product review management problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace AddressBookProblem_LINQ_DAY35
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review problem for LINQ");
+
+          
+        }
+    }
+}

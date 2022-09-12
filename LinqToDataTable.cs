e318e0ae9752665ca97742223fd24d88314abedb
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-12 15:11:34 +0530

    UC6_SkippingRecords

---
+++ LinqToDataTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ProductReviewManagement
+{
+    public class LinqToDataTable
+    {
+
+        public void AddDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Id");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "Chai");
+            table.Rows.Add("2", "Queso Cabrales");
+            table.Rows.Add("3", "Tofu");
+            DisplayProducts(table);
+        }
+
+
+        public void DisplayProducts(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
+            Console.WriteLine("ProductNames:");
+            foreach (string productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+    }
+}
---
+++ ProductManagement.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ProductReviewManagement
+{
+    public class ProductManagement
+    {
+
+        public readonly DataTable dataTable = new DataTable();
+        /// <summary>
+        /// UC2 - selected records with order by clause.
+        /// </summary>
+        /// <param name="review">The review.</param>
+        public void TopRecords(List<ProductReview> listReview)
+        {
+            //using query syntax
+            var recordedData = (from productReviews in listReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+            //using lambda syntax
+            var recordData = listReview.OrderByDescending(r => r.Rating).Take(3);
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
+            }
+        }
+
+
+        /// <summary>
+        /// Selected records with where condition UC3
+        /// </summary>
+        /// <param name="listReview">The list review.</param>
+        public void SelectedRecords(List<ProductReview> listReview)
+        {
+            //lambda syntax
+            var recordData = listReview.Where(r => r.Rating > 3 && (r.ProductId == 1 || r.ProductId == 4 || r.ProductId == 9)).ToList();
+            //query syntax
+            var recordedData = (from productReviews in listReview
+                                where productReviews.Rating > 3 && (productReviews.ProductId == 1 || productReviews.ProductId == 4 || productReviews.ProductId == 9)
+                                select productReviews);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
+            }
+        }
+
+        /// <summary>
+        /// UC4 - counts the of reviews grouping by user id
+        /// </summary>
+        /// <param name="listProductReview">The list product review.</param>
+        public void countOfReviews(List<ProductReview> listProductReview)
+        {
+            //lambda syntax
+            var recordData = listProductReview.GroupBy(r => r.UserId).Select(r => new { productId = r.Key, count = r.Count() });
+            //query syntax
+            var recordedData = from products in listProductReview
+                               group products by products.UserId into g
+                               select new
+                               {
+                                   userId = g.Key,
+                                   count = g.Count()
+                               };
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("UserId:-" + list.userId + " Count:-" + list.count);
+            }
+        }
+
+        /// <summary>
+        /// UC5 7 UC7 - Retrieves the product id and review.
+        /// </summary>
+        /// <param name="productReviewList">The product review list.</param>
+        public void retrieveProductIDandreview(List<ProductReview> productReviewList)
+        {
+            var recordData = productReviewList.Select(r => new { r.ProductId, r.Review });
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("product Id:-" + list.ProductId + " Review :-" + list.Review);
+            }
+        }
+
+
+        /// <summary>
+        /// Skippings the records. UC6
+        /// </summary>
+        /// <param name="productReviewList">The product review list.</param>
+        public void SkippingRecords(List<ProductReview> productReviewList)
+        {
+            var recordData = productReviewList.Skip(5);
+            var recordedData = (from products in productReviewList
+                                select products).Skip(5);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
+            }
+
+        }
+    }
+}
